10b4fb462e292ee155651ad71e617d3f7cb3a9f8
samanthagburek <123821380+samanthagburek@users.noreply.github.com>
2024-10-06 15:10:53 -0400

    Create productExceptSelf.py

---
+++ week1/productExceptSelf.py
@@ -0,0 +1,13 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        left = [1]*len(nums)
+        for i in range(1,len(nums)):
+            left[i] = left[i-1]*nums[i-1]
+        
+        right = [1]*len(nums)
+        for i in range(len(nums)-2, -1, -1):
+            right[i] = right[i+1]*nums[i+1]
+            
+        
+        answers = [left[i]*right[i] for i in range(len(nums))]
+        return answers
